da9242503e930648285abf87d11d9868ce352aa6
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-08-27 13:56:17 +0900

    Add view

---
+++ app/controllers/name_controller.rb
@@ -0,0 +1,4 @@
+class NameController < ApplicationController
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'name/create'
+
+  root 'top#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
