c020e822a232f10bd408f3370ac2cd54a00c12be
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-08-27 14:21:59 +0900

    Added form for index

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    @keyword = Keyword.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'name/create'
+  post 'name/create'
 
   root 'top#index'
 
